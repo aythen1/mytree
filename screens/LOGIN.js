799f7a18ff6a7c4c02c45cc2cbe664bf09314954
Mauricio <madegasperis@gmail.com>
2024-01-30 12:28:42 -0300

    fix: arreglos

--- screens/LOGIN.js
+++ screens/LOGIN.js
@@ -1,47 +1,54 @@
-import * as React from "react";
-import { Image } from "expo-image";
-import { StyleSheet, View, Pressable, Text } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
-import { useNavigation } from "@react-navigation/native";
-import { Color, FontFamily, Border, FontSize, Padding } from "../GlobalStyles";
+import React from 'react'
+import { Image } from 'expo-image'
+import {
+  StyleSheet,
+  View,
+  Pressable,
+  Text,
+  TextInput,
+  ScrollView
+} from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+import { useNavigation } from '@react-navigation/native'
+import { Color, FontFamily, Border, FontSize, Padding } from '../GlobalStyles'
 
 const LOGIN = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation()
 
   return (
-    <View style={styles.login}>
+    <ScrollView
+      style={styles.login}
+      contentContainerStyle={{ paddingBottom: 70 }}
+    >
       <LinearGradient
         style={[styles.loginInner, styles.buttonFlexBox]}
         locations={[0, 1]}
-        colors={["#7ec18c", "#dee274"]}
+        colors={['#7ec18c', '#dee274']}
       >
-        <View style={[styles.frameParent, styles.frameParentPosition]}>
+        <View>
           <Pressable
             style={styles.backButtonWrapper}
-            onPress={() => navigation.navigate("Splash")}
+            onPress={() => navigation.navigate('Splash')}
           >
             <Image
-              style={[styles.backButtonIcon, styles.frameParentPosition]}
+              style={styles.backButtonIcon}
               contentFit="cover"
-              source={require("../assets/back-button.png")}
+              source={require('../assets/back-button.png')}
             />
           </Pressable>
           <View style={styles.frameGroup}>
-            <View style={styles.estsDeVueltaParent}>
-              <Text style={[styles.estsDeVuelta, styles.signInClr]}>
-                ¡ESTÁS DE VUELTA!
-              </Text>
-              <Text style={[styles.teRecibimosCon, styles.signInClr]}>
-                Te recibimos con gusto. Tu compromiso impulsa la creación de
-                legados. Sigamos adelante, unidos en esta hermosa tarea.
-              </Text>
-            </View>
-            <Text style={[styles.ingresar, styles.signInClr]}>INGRESAR</Text>
+            <Text style={[styles.estsDeVuelta, styles.signInClr]}>
+              ¡ESTÁS DE VUELTA!
+            </Text>
+            <Text style={[styles.teRecibimosCon, styles.signInClr]}>
+              Te recibimos con gusto. Tu compromiso impulsa la creación de
+              legados. Sigamos adelante, unidos en esta hermosa tarea.
+            </Text>
           </View>
         </View>
       </LinearGradient>
       <View style={[styles.frameContainer, styles.checkChildBg]}>
-        <View>
+        <View style={styles.botContainer}>
           <Text style={[styles.labelled, styles.signInLayout]}>
             Correo Electrónico
           </Text>
@@ -50,31 +57,35 @@ const LOGIN = () => {
             <View
               style={[
                 styles.iconsEnvelopeSimpleParent,
-                styles.frameChildPosition,
+                styles.frameChildPosition
               ]}
             >
               <Image
                 style={styles.frameChildLayout}
                 contentFit="cover"
-                source={require("../assets/icons--envelope-simple.png")}
+                source={require('../assets/icons--envelope-simple.png')}
               />
-              <Image
-                style={styles.placeholderIcon}
-                contentFit="cover"
-                source={require("../assets/placeholder.png")}
-              />
+              <TextInput placeholder="|" style={styles.input} />
             </View>
           </View>
         </View>
-        <View style={styles.labelledGroup}>
+        <View style={styles.botContainer}>
           <Text style={[styles.labelled, styles.signInLayout]}>Contraseña</Text>
           <View style={styles.baseBackgroundParent}>
-            <View style={styles.baseLayout} />
-            <Image
-              style={[styles.frameChild, styles.frameChildLayout]}
-              contentFit="cover"
-              source={require("../assets/frame-1.png")}
-            />
+            <View style={[styles.baseBackground, styles.baseLayout]} />
+            <View
+              style={[
+                styles.iconsEnvelopeSimpleParent,
+                styles.frameChildPosition
+              ]}
+            >
+              <Image
+                style={styles.frameChildLayout}
+                contentFit="cover"
+                source={require('../assets/frame-1.png')}
+              />
+              <TextInput placeholder="••••••••" style={styles.input} />
+            </View>
           </View>
         </View>
         <View style={styles.frameView}>
@@ -93,228 +104,211 @@ const LOGIN = () => {
         <LinearGradient
           style={[styles.button, styles.buttonFlexBox]}
           locations={[0, 1]}
-          colors={["#dee274", "#7ec18c"]}
+          colors={['#dee274', '#7ec18c']}
         >
-          <Text style={[styles.signIn, styles.signInLayout]}>Ingresar</Text>
+          <Pressable onPress={() => navigation.navigate('MUROINFORMACIN')}>
+            <Text style={[styles.signIn, styles.signInLayout]}>Ingresar</Text>
+          </Pressable>
         </LinearGradient>
       </View>
-    </View>
-  );
-};
+    </ScrollView>
+  )
+}
 
 const styles = StyleSheet.create({
   buttonFlexBox: {
     backgroundColor: Color.linearBoton,
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "row",
-  },
-  frameParentPosition: {
-    zIndex: 0,
-    position: "absolute",
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row'
   },
   signInClr: {
     color: Color.white,
-    textAlign: "center",
+    textAlign: 'center'
   },
   checkChildBg: {
-    backgroundColor: Color.white,
-    position: "absolute",
+    backgroundColor: Color.white
   },
   signInLayout: {
     lineHeight: 24,
-    fontFamily: FontFamily.lato,
+    fontFamily: FontFamily.lato
   },
   baseLayout: {
     height: 50,
     backgroundColor: Color.fAFAFA,
     borderRadius: Border.br_3xs,
-    width: 388,
-    zIndex: 0,
+
+    zIndex: 0
   },
   frameChildPosition: {
-    zIndex: 1,
-    position: "absolute",
+    zIndex: 1
   },
   frameChildLayout: {
     height: 24,
-    width: 24,
+    width: 24
   },
   labelled2Typo: {
     fontSize: FontSize.size_xs,
-    fontFamily: FontFamily.lato,
+    fontFamily: FontFamily.lato
   },
   backButtonIcon: {
-    top: 33,
-    left: 26,
+    position: 'absolute',
+    top: 5,
+    left: 30,
     width: 18,
-    height: 0,
+    height: 10
   },
   backButtonWrapper: {
     padding: Padding.p_xl,
-    alignItems: "flex-end",
-    justifyContent: "center",
+    alignItems: 'flex-end',
+    justifyContent: 'center'
   },
   estsDeVuelta: {
     fontSize: FontSize.size_5xl,
-    textAlign: "center",
+    textAlign: 'center',
     fontFamily: FontFamily.lato,
     color: Color.white,
-    fontWeight: "900",
-    lineHeight: 24,
+    fontWeight: '900',
+    lineHeight: 24
   },
   teRecibimosCon: {
     lineHeight: 20,
-    fontWeight: "700",
-    display: "flex",
+    fontWeight: '700',
+    display: 'flex',
     width: 286,
     marginTop: 30,
     fontSize: FontSize.size_sm,
-    textAlign: "center",
+    textAlign: 'center',
     fontFamily: FontFamily.lato,
     color: Color.white,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  estsDeVueltaParent: {
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   ingresar: {
     fontSize: FontSize.size_lg,
-    textTransform: "uppercase",
+    textTransform: 'uppercase',
     marginTop: 98,
-    textAlign: "center",
+    textAlign: 'center',
     fontFamily: FontFamily.lato,
     color: Color.white,
-    fontWeight: "900",
-    lineHeight: 24,
+    fontWeight: '900',
+    lineHeight: 24
   },
   frameGroup: {
     paddingLeft: 47,
     paddingRight: 55,
-    marginTop: 97,
-    alignItems: "center",
-    width: 428,
-  },
-  frameParent: {
-    marginLeft: -214,
-    left: "50%",
-    height: 434,
-    width: 428,
-    top: 0,
+    alignItems: 'center',
+    width: '100%'
   },
   loginInner: {
-    width: 428,
+    width: '100%',
+    height: '40%',
     left: 0,
     top: 0,
-    position: "absolute",
-    backgroundColor: Color.linearBoton,
+    backgroundColor: Color.linearBoton
   },
   labelled: {
     color: Color.negro,
-    width: 388,
-    textAlign: "left",
+    width: '100%',
+    textAlign: 'left',
     fontSize: FontSize.size_sm,
-    fontWeight: "900",
+    fontWeight: '900'
   },
   baseBackground: {
-    shadowColor: "rgba(244, 105, 76, 0.15)",
+    shadowColor: 'rgba(244, 105, 76, 0.15)',
     shadowOffset: {
       width: 0,
-      height: 0,
+      height: 0
     },
     shadowRadius: 14,
     elevation: 14,
-    shadowOpacity: 1,
+    shadowOpacity: 1
   },
   placeholderIcon: {
-    maxWidth: "100%",
-    overflow: "hidden",
     height: 22,
-    marginLeft: 16,
+    marginLeft: 16
   },
   iconsEnvelopeSimpleParent: {
-    top: 15,
     left: 16,
-    flexDirection: "row",
+    flexDirection: 'row',
+    bottom: 37
   },
   baseBackgroundParent: {
-    marginTop: 11,
+    marginTop: 11
   },
   frameChild: {
-    top: 14,
     left: 15,
-    zIndex: 1,
-    position: "absolute",
-  },
-  labelledGroup: {
-    marginTop: 33,
+    zIndex: 1
   },
   checkChild: {
-    height: "105%",
-    width: "105%",
-    top: "-2.5%",
-    right: "-2.5%",
-    bottom: "-2.5%",
-    left: "-2.5%",
+    height: '105%',
+    width: '105%',
+    top: '-2.5%',
+    right: '-2.5%',
+    bottom: '-2.5%',
+    left: '-2.5%',
     borderRadius: 3,
-    borderStyle: "solid",
+    borderStyle: 'solid',
     borderColor: Color.colorGainsboro_100,
-    borderWidth: 1,
+    borderWidth: 1
   },
   check: {
     width: 20,
-    height: 20,
+    height: 20
   },
   labelled2: {
     lineHeight: 20,
     color: Color.neutralGray1,
     marginLeft: 20,
-    textAlign: "left",
+    textAlign: 'left'
   },
   checkParent: {
-    flexDirection: "row",
+    flexDirection: 'row'
   },
   olvidasteTuContrasea: {
     lineHeight: 18,
-    fontWeight: "500",
+    fontWeight: '500',
     color: Color.primario1,
-    marginLeft: 148,
-    textAlign: "center",
+    marginLeft: '25%',
+    textAlign: 'center'
   },
   frameView: {
     marginTop: 33,
-    alignItems: "flex-end",
-    flexDirection: "row",
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    width: '100%'
   },
   signIn: {
     fontSize: FontSize.size_base,
     letterSpacing: 1,
-    textAlign: "center",
+    textAlign: 'center',
     color: Color.white,
-    flex: 1,
+    flex: 1
   },
   button: {
     borderRadius: Border.br_11xl,
     paddingHorizontal: Padding.p_5xl,
     paddingVertical: Padding.p_sm,
-    marginTop: 33,
-    width: 388,
+    width: '100%',
+    top: 20
   },
   frameContainer: {
-    top: 434,
-    height: 492,
+    height: '100%',
     padding: Padding.p_xl,
-    alignItems: "center",
-    width: 428,
-    left: 0,
+    alignItems: 'center',
+    width: '100%',
+    left: 0
   },
   login: {
-    borderRadius: Border.br_21xl,
-    width: "100%",
-    height: 926,
-    flex: 1,
+    height: '150%',
+    flex: 1
   },
-});
+  input: {
+    left: 10
+  },
+  botContainer: {
+    width: '100%'
+  }
+})
 
-export default LOGIN;
+export default LOGIN
