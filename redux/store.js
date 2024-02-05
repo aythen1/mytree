4d1a74d153270e5b7cf90165365bd996ec1cd846
CristianPerez <norberto.cp@hotmail.com>
2024-02-05 11:03:11 -0300

    feat: crear id infante

--- redux/store.js
+++ redux/store.js
@@ -3,12 +3,14 @@ import storiesSlices from './slices/stories.slices'
 import challengersSlices from './slices/challengers.slices'
 import panelSlices from './slices/panel.slices'
 import memoriesSlices from './slices/memories.slices'
+import infantSlices from './slices/infant.slices'
 
 export const store = configureStore({
   reducer: {
     stories: storiesSlices,
     challengers: challengersSlices,
     panel: panelSlices,
-    memories: memoriesSlices
+    memories: memoriesSlices,
+    infants: infantSlices
   }
 })
